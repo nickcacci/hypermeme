f745845053a5002890d0587eceb22263bbabf594
nickcacciola <nikocacci@gmail.com>
2024-09-23 21:33:21 +0200

    Fixed gpu use in cpu compose torch use cuda

--- praw-python/bin/praw_python.py
+++ praw-python/bin/praw_python.py
@@ -55,9 +55,14 @@ class meme:
 
     reader = easyocr.Reader(["en"])
     processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
-    model = BlipForConditionalGeneration.from_pretrained(
-        "Salesforce/blip-image-captioning-base", torch_dtype=torch.float16
-    ).to("cuda")
+    if bool(os.environ.get("TAP_USE_GPU", False)):
+        model = BlipForConditionalGeneration.from_pretrained(
+            "Salesforce/blip-image-captioning-base", torch_dtype=torch.float16
+        ).to("cuda")
+    else:
+        model = BlipForConditionalGeneration.from_pretrained(
+            "Salesforce/blip-image-captioning-base"
+        )
 
     def __init__(self, submission: praw.models.Submission):
 
